7d206fa0151b0e9ec947d4d7cb0029153f0375c5
Adam Bergman <adam@fransvilhelm.com>
2017-09-01 12:24:25 +0200

    :art: Add basic api to get news

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var api = require('./routes/api');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const api = require('./routes/api');
 
 const PUBLIC_DIR = process.env.NODE_ENV === 'production' ? 'build' : 'public';
 
-var app = express();
+const app = express();
 
 app.use(favicon(path.join(__dirname, PUBLIC_DIR, 'favicon.ico')));
 app.use(logger('dev'));
@@ -22,7 +24,7 @@ app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
